ddedaea5ff1901eea3e12d663ddb0bd235a165c8
Ben Coomes <bencoomes@github.com>
2022-12-02 09:00:24 -0500

    day 2

---
+++ 2022/day2/scorer.rb
@@ -0,0 +1,89 @@
+module RPS
+  class Item
+    ITEM_TO_OUTCOME = {
+      rock: { rock: :draw, paper: :lose, scissor: :win },
+      paper: { rock: :win, paper: :draw, scissor: :lose },
+      scissor: { rock: :lose, paper: :win, scissor: :draw }
+    }.freeze
+
+    OUTCOME_TO_ITEM_TYPE = {
+      rock: { win: :paper, draw: :rock, lose: :scissor },
+      paper: { win: :scissor, draw: :paper, lose: :rock },
+      scissor: { win: :rock, draw: :scissor, lose: :paper }
+    }
+
+    POINTS_FOR_OUTCOME = {
+      win: 6,
+      draw: 3,
+      lose: 0
+    }.freeze
+
+    POINTS_FOR_TYPE = {
+      rock: 1,
+      paper: 2,
+      scissor: 3
+    }.freeze
+
+    ITEM_TYPE_FOR = {
+      A: :rock,
+      X: :rock,
+      B: :paper,
+      Y: :paper,
+      C: :scissor,
+      Z: :scissor
+    }.freeze
+
+    OUTCOME_FOR = {
+      X: :lose,
+      Y: :draw,
+      Z: :win
+    }.freeze
+
+    attr_reader :type
+
+    def self.from_symbol(sym)
+      Item.new(ITEM_TYPE_FOR[sym])
+    end
+
+    def initialize(type)
+      @type = type
+    end
+
+    def points_against(other)
+      POINTS_FOR_OUTCOME[ITEM_TO_OUTCOME[@type][other.type]]
+    end
+
+    def choice_for_result(result)
+      Item.new(OUTCOME_TO_ITEM_TYPE[@type][result])
+    end
+
+    def inherrent_worth
+      POINTS_FOR_TYPE[@type]
+    end
+  end
+end
+
+def part1
+  total_score = 0
+  File.foreach('input.txt') do |line|
+    opp, you = line.split(' ').map { |x| RPS::Item.from_symbol(x.to_sym) } 
+    score = you.points_against(opp) + you.inherrent_worth
+    total_score += score
+  end
+  puts total_score
+end
+
+def part2
+  total_score = 0
+  File.foreach('input.txt') do |line|
+    opp, result = line.split(' ')
+    opp = RPS::Item.from_symbol(opp.to_sym)
+    result = RPS::Item::OUTCOME_FOR[result.to_sym]
+    you = opp.choice_for_result(result)
+    total_score += you.points_against(opp) + you.inherrent_worth
+  end
+  puts total_score
+end
+
+part1
+part2
