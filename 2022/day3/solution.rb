ad31fe4881ca4933e8ed732b5354dda3049eb9ab
Ben Coomes <bencoomes@github.com>
2022-12-03 09:38:53 -0500

    2022 day 3

---
+++ 2022/day3/solution.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal:true
+
+class Bagpack
+  PRIORITIES = Array('a'..'z').concat(Array('A'..'Z')).zip(1..52).to_h
+
+  attr_reader :items, :comp1, :comp2
+
+  def initialize(items)
+    @items = items.rstrip.chars
+    @item_count = items.size
+    split_index = @item_count / 2
+    @comp1 = @items[0...split_index]
+    @comp2 = @items[split_index..]
+  end
+
+  def items_in_both_compartments
+    comp1 & comp2
+  end
+
+  def shared_items(other_packs = [])
+    shared = items
+    other_packs.each { |p| shared &= p.items }
+    shared
+  end
+
+  def self.priority_sum(items)
+    items.map { |i| PRIORITIES[i] }.sum
+  end
+end
+
+def part1(file)
+  puts "part 1 (#{file}):"
+
+  sum = 0
+  File.foreach(file) do |line|
+    bp = Bagpack.new(line)
+    sum += Bagpack.priority_sum(bp.items_in_both_compartments)
+  end
+  puts sum
+end
+
+def part2(file)
+  puts "part 2 (#{file}):"
+
+  packs = []
+  File.foreach(file) do |line|
+    packs << Bagpack.new(line)
+  end
+
+  groups = packs.each_slice(3).to_a
+  sum = 0
+  groups.each do |g|
+    shared = g.first.shared_items(g.slice(1..))
+    sum += Bagpack.priority_sum(shared)
+  end
+  puts sum
+end
+
+part1('sample.txt')
+part1('input.txt')
+
+part2('sample.txt')
+part2('input.txt')
