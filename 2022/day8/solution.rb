2a483a57b78cb314c69cbf0fa77400317e2bc30f
Ben Coomes <bencoomes@github.com>
2022-12-08 08:48:42 -0500

    2022 day 8 part 2

--- 2022/day8/solution.rb
+++ 2022/day8/solution.rb
@@ -37,8 +37,32 @@ def part1(file)
 end
 
 def part2(file)
-  File.foreach(file) do |line|
-    # part 2 solution here
+  max_score = 0
+  tree_rows, tree_cols = parse_trees(file)
+
+  tree_rows.each_with_index do |row, row_index|
+    next if row_index.zero? || row_index == tree_rows.size - 1
+
+    row.each_with_index do |tree, col_index|
+      next if col_index.zero? || col_index == row.size - 1
+
+      to_right = tree_rows[row_index][col_index+1...]
+      rvd = (to_right.index { |t| t >= tree } || to_right.size - 1) + 1
+
+      to_left = tree_rows[row_index][0...col_index].reverse
+      lvd = (to_left.index { |t| t >= tree } || to_left.size - 1) + 1
+
+      above = tree_cols[col_index][0...row_index].reverse
+      avd = (above.index { |t| t >= tree } || above.size - 1) + 1
+
+      below = tree_cols[col_index][row_index+1...]
+      bvd = (below.index { |t| t >= tree } || below.size - 1) + 1
+
+      score = rvd * lvd * avd * bvd
+      if score > max_score
+        max_score = score
+      end
+    end
   end
-  :no_answer
+  max_score
 end
