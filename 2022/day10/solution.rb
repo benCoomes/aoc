774a21434041f0267e0372d68d577f53ab2eaa82
Ben Coomes <bencoomes@github.com>
2022-12-10 09:51:53 -0500

    2022 day 10 part 2

--- 2022/day10/solution.rb
+++ 2022/day10/solution.rb
@@ -45,9 +45,47 @@ def part1(file)
   sum
 end
 
+
+def print_screen(cycle, reg)
+  pos = (cycle-1) % 40
+  pixel = ([reg-1, reg, reg+1].include? pos) ? '#' : ' '
+  print pixel
+  if cycle % 40 == 0
+    puts "\n"
+  end
+end
+
 def part2(file)
+  sum = 0
+  reg = 1
+  cycle = 1
+
   File.foreach(file) do |line|
-    # part 2 solution here
+    op, val = line.split(' ')
+
+    case op
+    when "addx"
+      # first part
+
+      # during - check pos & draw
+      print_screen(cycle, reg)
+
+      # after - increment cycle
+      cycle += 1
+
+      # second part 
+
+      # during - check pos & draw
+      print_screen(cycle, reg)
+
+      # after - increment cycle and reg
+      cycle += 1
+      reg += val.to_i
+    when "noop"
+      # during - check pos & draw
+      print_screen(cycle, reg)
+      cycle += 1
+    end
   end
   :no_answer
 end
