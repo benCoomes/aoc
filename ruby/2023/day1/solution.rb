5c61a054d2a9be14f41d89287f49085fd7058bf2
Ben Coomes <bencoomes@github.com>
2023-12-02 10:27:24 -0500

    move existing files into ruby folder

---
+++ ruby/2023/day1/solution.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal:true
+
+def part1(file)
+  File.foreach(file) do |line|
+    # part 1 solution here
+  end
+  :no_answer
+end
+
+def part2(file)
+  File.foreach(file) do |line|
+    # part 2 solution here
+  end
+  :no_answer
+end
